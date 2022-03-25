d29da482ec866eea6943708997268fbc2d2c8b88
Omar Rojas-Padilla <projasomar@gmail.com>
2022-03-25 17:25:01 -0300

    Update downseque.R
    script más limpio.
    -- Cuando algún día aprenda a hacer loops, pues lo actualizaré. Hasta eso agradezco si alguien lo hace.
    :)

--- downseque.R
+++ downseque.R
@@ -1,6 +1,9 @@
 ## Modificado de Vikram B Baliga: https://www.vikram-baliga.com/blog/2016/1/23/using-r-to-download-genbank-sequences y de Juan C. Santos: http://www.jcsantosresearch.org/Class_2014_Spring_Comparative/pdf/week_2/Jan_13_15_2015_GenBank_part_2.pdf
 
-## Secuencias de ejemplo: Ctenophryne
+## Secuencias de ejemplo: Chiasmocleis
+
+##este script necesita un archivo .csv donde informes el nombre que quieres darle a la secuencia y los códigos de GenBank de la secuencia de interés. Es importante uniformizar los nombres de las secuencias para poder concatenarlas después
+###en el link de Git-Hub hay un ejemplo de como debe ser el csv
 
 # llamar paquetes
 library(ape)
@@ -9,41 +12,69 @@ library(seqinr)
 sessionInfo() #comprobar que los paquetes cargaron
 
 # definir carpeta de trabajo
-setwd("D:/tu carpeta de trabajo")
+setwd()
 dir() #revisar archivos en la carpeta de trabajo
+#el .csv con los datos deben estar en esta carpeta de trabajo.
 
 # importar archivos .csv
-cten <- read.csv("example.csv", sep = ";", quote = "\"", head = TRUE, stringsAsFactors = FALSE)
-#el sep = ";" puede cambiarse por ",". Esto depende de la configuración del computador con respecto a qué caracter usa como separador de columnas en el csv.
+example <- read.csv("example.csv", sep = ";", quote = "\"", head = TRUE, stringsAsFactors = FALSE) #sep puede variar a "," de acuerdo a la configuración del computador
+head(example)
 
-#### BAJAR SECUENCIAS
+## estas líneas seleccionan relaciona las columnas que contienen el código de la secuencia con el nombre que se dará a la secuencia. 
 
-##16S
+###16s
+##filtrar NA
+id.16S <- example.frame(example$a16S, example$Seq_name) #match entre códigos de GB y Nombre secuencias
+id.16S <- na.omit(id.16S) ##eliminar los nombres de secuencia que no están asociadas a ningún código de GB
+GB.16S <- id.16S$example.a16S ##codigos de GB después de eliminar los NA
+name.16S <- id.16S$example.Seq_name ##nombres de secuencia después de NA
 
-#bajar secuencias
-cten.16<-read.GenBank(cten$X16S,species.names=T)
-#Definir nombre de archivos para fasta final
-cten.16.id <- paste(attr(cten.16, "species"), cten$Museo, sep = "_")
-#crear primer archivo fasta
-write.dna(cten.16, file ="cten.16_temp.fasta", format = "fasta", append = FALSE, nbcol = 6, colsep = " ", colw = 10)
-##cargar primer fasta
-cten.16.temp <- read.fasta(file = "cten.16_temp.fasta", seqtype = "DNA", as.string = TRUE, forceDNAtolower = FALSE)
-#reescribir fasta
-write.fasta(sequences = cten.16.temp, names = cten.16.id, nbchar = 10, file.out = "Ctenophryne_16S.fasta")
+##bajar secuencias de GB
+Chias.16S <- read.GenBank(GB.16S, species.names = F) ##secuencias de GB pero sin el nombre que le dimos
+write.dna(Chias.16S, file = "Chias.16S_temp.fasta", format = "fasta", append = F, nbcol = 5, colsep = " ", colw = 10) #creamos archivo temporal.
+Chias.16S.temp <- read.fasta(file = "Chias.16S_temp.fasta", seqtype = "DNA", as.string = TRUE, forceDNAtolower = FALSE) #cargamos archivo temporal
+write.fasta(sequences = Chias.16S.temp, names = name.16S, nbchar = 10, file.out = "Chiasmocleis_16S.fasta")
 
-## BAJAR COI
-#como esta secuencia tiene NA, primero se tiene que eliminar.
+###COI
+##puedes pegar el script en notepad++ y cambiar estos parámetros
+#a16S --> COI
+#16S --> COI
+##y repetir el proceso según los marcadores que tengas, recuerda que esto está relacionado al nombre que pusiste en el archivo .csv
 
-#########################eliminar NA en tabla
-#1. Separar columnas de GEN y TOMBO
-id.coi <- data.frame(cten$COI, cten$Museo)
-#2. Eliminar NA en TOMBO y GEN
-id.coi <- na.omit(id.coi)
-#############################################
+id.COI <- example.frame(example$COI, example$Seq_name)
+id.COI <- na.omit(id.COI)
+GB.COI <- id.COI$example.COI
+name.COI <- id.COI$example.Seq_name
 
-cten.coi <- read.GenBank(id.coi$cten.COI,species.names=T)
-cten.coi.id <- paste(attr(cten.coi, "species"), id.coi$cten.Museo, sep = "_")
-write.dna(cten.coi, file ="cten.coi_temp.fasta", format = "fasta", append = FALSE, nbcol = 6, colsep = " ", colw = 10)
-cten.coi.temp <- read.fasta(file = "cten.coi_temp.fasta", seqtype = "DNA", as.string = TRUE, forceDNAtolower = FALSE)
-write.fasta(sequences = cten.coi.temp, names = cten.coi.id, nbchar = 10, file.out = "Ctenophryne_COI.fasta")
+Chias.COI <- read.GenBank(GB.COI, species.names = F)
+write.dna(Chias.COI, file = "Chias.COI_temp.fasta", format = "fasta", append = F, nbcol = 5, colsep = " ", colw = 10)
+Chias.COI.temp <- read.fasta(file = "Chias.COI_temp.fasta", seqtype = "DNA", as.string = TRUE, forceDNAtolower = FALSE)
+write.fasta(sequences = Chias.COI.temp, names = name.COI, nbchar = 10, file.out = "Chiasmocleis_COI.fasta")
 
+### BDNF
+#COI --> BDNF
+
+id.BDNF <- example.frame(example$BDNF, example$Seq_name)
+id.BDNF <- na.omit(id.BDNF)
+GB.BDNF <- id.BDNF$example.BDNF
+name.BDNF <- id.BDNF$example.Seq_name
+
+Chias.BDNF <- read.GenBank(GB.BDNF, species.names = F)
+write.dna(Chias.BDNF, file = "Chias.BDNF_temp.fasta", format = "fasta", append = F, nbcol = 5, colsep = " ", colw = 10)
+Chias.BDNF.temp <- read.fasta(file = "Chias.BDNF_temp.fasta", seqtype = "DNA", as.string = TRUE, forceDNAtolower = FALSE)
+write.fasta(sequences = Chias.BDNF.temp, names = name.BDNF, nbchar = 10, file.out = "Chiasmocleis_BDNF.fasta")
+
+### TYR
+#BDNF --> TYR
+id.TYR <- example.frame(example$TYR, example$Seq_name)
+id.TYR <- na.omit(id.TYR)
+GB.TYR <- id.TYR$example.TYR
+name.TYR <- id.TYR$example.Seq_name
+
+Chias.TYR <- read.GenBank(GB.TYR, species.names = F)
+write.dna(Chias.TYR, file = "Chias.TYR_temp.fasta", format = "fasta", append = F, nbcol = 5, colsep = " ", colw = 10)
+Chias.TYR.temp <- read.fasta(file = "Chias.TYR_temp.fasta", seqtype = "DNA", as.string = TRUE, forceDNAtolower = FALSE)
+write.fasta(sequences = Chias.TYR.temp, names = name.TYR, nbchar = 10, file.out = "Chiasmocleis_TYR.fasta")
+
+## para citar paquetes y al R usar el comando: citation()
+##link git-hub https://github.com/orojas91/Secuencias-Genbank-con-R
